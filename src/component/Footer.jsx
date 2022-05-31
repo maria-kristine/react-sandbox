95303b073cd9ff0b7758c86491b95f0b10594672
maria-kristine <m-kpedersen@hotmail.com>
2022-05-31 13:11:14 +0200

    add weather component and adress

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import Weather from "./Weather";
 
 
 
@@ -8,15 +9,30 @@ const Footer = () => {
     const styles = {
         footer: css`
         background-color: #252734;
-        padding: 35px;
+        padding: 40px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        & h4 {
+            color: #fff;
+            font-weight: normal;
+            font-size: 0.80rem;
+        }
         & p {
             color: #fff;
+            font-size: 0.80rem;
         }
         `,
     }
     return ( 
         <footer css={styles.footer}>
-            <p>weather</p>
+            <div>
+                <h4>Sandbox</h4>
+                <p>Copenhagengade 100, 3. sal</p>
+                <p>2200 Copenhagen K</p>
+                <p>Denmark</p>
+            </div>
+           <Weather/>
         </footer>
      );
 }
