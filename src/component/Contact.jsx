e8cb5bcc559511bc496e6530020632c42852961e
maria-kristine <m-kpedersen@hotmail.com>
2022-05-31 13:11:55 +0200

    add contact form component to contact component

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import ContactForm from "./Contact-form";
 
 const Contact = () => {
   const styles = {
@@ -14,62 +15,12 @@ const Contact = () => {
          color: #252734;
       }
     `,
-    label: css`
-      text-align: left;
-      display: block;
-    `,
-     input: css`
-     width: 100%;
-     padding: 6px 10px;
-     margin: 10px 0;
-     border: 1px solid #ddd;
-     box-sizing: border-box;
-     display: block;
-   `,
-    textarea: css`
-      width: 100%;
-      height: 100px;
-      padding: 6px 10px;
-      margin: 10px 0;
-      border: 1px solid #ddd;
-      box-sizing: border-box;
-      display: block;
-      resize: none;
-    `,
-     button: css`
-     background: #9ec4dc;
-     color: #fff;
-     border: 0;
-     padding: 8px 30px;
-     border-radius: 8px;
-     cursor: pointer;
-   `,
+
   };
   return (
     <div css={styles.contact}>
       <h2>Contact us</h2>
-      <form onSubmit>
-        <label css={styles.label}>
-          First name:
-          <input css={styles.input} type="text" 
-          required />
-        </label>
-        <label css={styles.label}>
-          Last name:
-          <input css={styles.input} type="text" 
-          required />
-        </label>
-        <label css={styles.label}>
-          Email:
-          <input css={styles.input} type="text" 
-          required />
-        </label>
-        <label css={styles.label}>
-          Message:
-          <textarea css={styles.textarea} required></textarea>
-        </label>
-        <button css={styles.button}>Send</button>
-      </form>
+        <ContactForm/>
     </div>
   );
 };
