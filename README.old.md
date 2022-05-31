e344bcf787bcad8f3edbf6276fa3410bbfe7e453
maria-kristine <m-kpedersen@hotmail.com>
2022-05-31 13:12:06 +0200

    update

--- README.old.md
+++ README.old.md
@@ -1,1 +1,14 @@
 # react-sandbox
+
+1. Hello World x
+2. Introducing JSX x
+3. Rendering Elements x
+4. Components and Props
+5. State and Lifecycle
+6. Handling Events x
+7. Conditional Rendering x
+8. Lists and Keys
+9. Forms x
+10. Lifting State Up
+11. Composition vs Inheritance
+12. Thinking In React x
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Contact from './component/Contact';
-import App from './App';
-import About from './component/About';
+import Contact from "./component/Contact";
+import App from "./App";
+import About from "./component/About";
+import Home from "./component/Home";
+import MessageSend from "./component/Message-send";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App/>}>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/about" element={<About/>}/>
+        <Route path="/" element={<App />}>
+          <Route path="/home" element={<Home/>} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/thanks" element={<MessageSend />} />
         </Route>
       </Routes>
     </BrowserRouter>
