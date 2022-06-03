6d94822dee67102c55ac4548f7c40b361b46d3eb
maria-kristine <m-kpedersen@hotmail.com>
2022-06-03 08:37:39 +0200

    add home component

---
+++ src/component/Home.jsx
@@ -0,0 +1,24 @@
+import Slideshow from "./Slideshow";
+
+const Home = () => {
+  return (
+    <div>
+      <Slideshow />
+      <h2>Sandbox</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+        tempore fuga voluptatem atque, mollitia temporibus libero ducimus.
+        Placeat deserunt, voluptatum molestiae, cumque at quod neque velit iure
+        sapiente adipisci ducimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+        tempore fuga voluptatem atque, mollitia temporibus libero ducimus.
+        Placeat deserunt, voluptatum molestiae, cumque at quod neque velit iure
+        sapiente adipisci ducimus.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
