161d95374d4689213de5a33129bb372fce290ca7
maria-kristine <m-kpedersen@hotmail.com>
2022-06-03 19:48:46 +0200

    update styling

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,10 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import Slideshow from "./Slideshow";
 
 const Home = () => {
+
+    const styles = {
+        home: css`
+        & h2{
+            text-align: center;
+            font-size: 2rem;
+            padding: 20px 0;
+        }
+
+        & p{
+            padding: 0 30px 10px 30px;
+            text-align: justify;
+        }
+        `,
+    }
   return (
-    <div>
+    <div css={styles.home}>
       <Slideshow />
       <h2>Sandbox</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+        tempore fuga voluptatem atque, mollitia temporibus libero ducimus.
+        Placeat deserunt, voluptatum molestiae, cumque at quod neque velit iure
+        sapiente adipisci ducimus.
+      </p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
         tempore fuga voluptatem atque, mollitia temporibus libero ducimus.
